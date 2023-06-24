0d086b0468a01d635d23e3e9136ee15b959dc4af
timofey7lam <123180174+timofey7lam@users.noreply.github.com>
2023-06-24 21:19:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+function openModal(imageSrc) {
+  const modal = document.getElementById('modal');
+  const modalImage = document.getElementById('modalImage');
+  modalImage.src = imageSrc;
+  modal.style.display = 'flex';
+}
+
+function closeModal() {
+  const modal = document.getElementById('modal');
+  modal.style.display = 'none';
+}
+
+window.onclick = function(event) {
+  const modal = document.getElementById('modal');
+  if (event.target === modal) {
+    closeModal();
+  }
+};
+
+const imgBox = document.querySelector('.imgBox');
+const slides = imgBox.getElementsByTagName('img');
+var i = 0;
+
+function prevSlide() {
+    slides[i].classList.remove('active');
+    i = (i - 1 + slides.length) % slides.length;
+    slides[i].classList.add('active');
+}
+
+function nextSlide() {
+    slides[i].classList.remove('active');
+    i = (i + 1) % slides.length;
+    slides[i].classList.add('active');
+}
+
+const contentBox = document.querySelector('.contentBox');
+const slidesText = contentBox.getElementsByTagName('div');
+var j = 0;
+
+function prevSlideText() {
+    slidesText[j].classList.remove('active');
+    j = (j - 1 + slidesText.length) % slidesText.length;
+    slidesText[j].classList.add('active');
+}
+
+function nextSlideText() {
+    slidesText[j].classList.remove('active');
+    j = (j + 1) % slidesText.length;
+    slidesText[j].classList.add('active');
+}
