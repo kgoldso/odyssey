bab25960f3f78c6d1a72a686fac4f44bbd25eb96
timofey7lam <123180174+timofey7lam@users.noreply.github.com>
2023-06-27 22:49:20 +0300

    Add files via upload

---
+++ animations.js
@@ -0,0 +1,15 @@
+ScrollReveal({ reset: true });
+ScrollReveal().reveal('.fact-title', { easing: 'ease-in' });
+ScrollReveal().reveal('.gradient-text-cosmo', { easing: 'ease-in' });
+ScrollReveal().reveal('.fact-block', { easing: 'ease-in' });
+ScrollReveal().reveal('.space-travel-block', { easing: 'ease-in' });
+ScrollReveal().reveal('.text-photo', { easing: 'ease-in' });
+ScrollReveal().reveal('.image-grid', { easing: 'ease-in' });
+ScrollReveal().reveal('.contentBox', { easing: 'ease-in' });
+ScrollReveal().reveal('.controls', { easing: 'ease-in' });
+ScrollReveal().reveal('.footer-content', { easing: 'ease-in' });
+
+
+
+
+// ScrollReveal().reveal('.', { easing: 'ease-in' });
